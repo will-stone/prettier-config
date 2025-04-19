e3bdf9fb7edd6a24025c5ee193f992aae53ac57b
Will Stone <654103+will-stone@users.noreply.github.com>
2025-04-19 11:35:28 +0100

    Add linting

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/* eslint-disable unicorn/prefer-module */
+
 module.exports = {
+  plugins: [require.resolve('prettier-plugin-packagejson')],
   proseWrap: 'always',
   semi: false,
   singleQuote: true,
-  plugins: [require.resolve('prettier-plugin-packagejson')],
 }
