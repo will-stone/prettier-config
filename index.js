5b3af6c35923ec30a1864f0f46e419fe8ba04d9d
Will Stone <654103+will-stone@users.noreply.github.com>
2026-02-07 13:15:27 +0000

    ! Change print width to 100

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-export default {
+// @ts-check
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 100,
   proseWrap: 'always',
   semi: false,
   singleQuote: true,
 }
+
+export default config
